51a600b2eb569c650d98de7d91ce03c44ae7ea30
CarlosAlfredoOliveiraDeLima <carlos.alfredo.oliveira.de.lima@gmail.com>
2023-09-29 22:06:58 -0300

    Otimizacao script com tflite

--- app_tflite.py
+++ app_tflite.py
@@ -4,7 +4,6 @@ import time
 import tensorflow as tf
 from keras.models import load_model
 
-KCmodel_path = "ml-models/model_unquant.tflite"
 
 # Substitua 'seu_modelo.tflite' pelo caminho para o seu arquivo .tflite
 modelo_tflite = 'ml-models/model_unquant.tflite'
@@ -14,6 +13,7 @@ interpreter.allocate_tensors()
 # Obtenha os detalhes do tensor de entrada e saída
 entrada_details = interpreter.get_input_details()
 saida_details = interpreter.get_output_details()
+data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
 
 video = cv.VideoCapture('videos/carPark.mp4')
 
@@ -21,7 +21,11 @@ if not video.isOpened():
     print('Erro ao carregar o vídeo.')
     exit()
 
+frame_pular = 6
+frame_count = 0
 while True:
+    red_count = 0
+    green_count = 0
     ret, image = video.read()
     #Rotina para verificar se o vídeo chegou ao fim e então terminar o loop ou reiniciar o vídeo
     if not ret:
@@ -41,67 +45,135 @@ while True:
     put_text_x2 = 113
     put_text_y = 15
     
-    for i in range(1, 13, 1):
-        #Coluna A
-        cv.rectangle(image, (x1,y1), (x2,y2), (0,0,255), 2)
-        crop_A = cv.resize(image[y1:y2, x1:x2], (224, 224))
-
-        #Coluna B
-        crop_B = cv.resize(image[y1:y2, x2:x2+length_parking_space], (224, 224))
-        crop_B = cv.cvtColor(crop_B, cv.COLOR_BGR2RGB)
-        crop_B = np.expand_dims(crop_B, axis=0)
-        crop_B = crop_B.astype(entrada_details[0]['dtype'])
-        # Alimente a imagem para o modelo
-        interpreter.set_tensor(entrada_details[0]['index'], crop_B)
-        # Execute a inferência
-        interpreter.invoke()
-        # Obtenha as saídas do modelo
-        saida = interpreter.get_tensor(saida_details[0]['index'])
-        # Verifique a classe com maior probabilidade
-        classe_predita = np.argmax(saida)
-        if classe_predita == 0:
-            cv.rectangle(image, (x2,y1), (x2+length_parking_space,y2), (0,0,255), 2)
-        elif classe_predita == 1:
-            cv.rectangle(image, (x2,y1), (x2+length_parking_space,y2), (0,255,0), 2)
+    if frame_count == 1 or frame_count % frame_pular == 0:
+        for i in range(1, 13, 1):  
+            #Coluna A
+            cv.rectangle(image, (x1,y1), (x2,y2), (0,0,255), 2)
+            crop_A = cv.resize(image[y1:y2, x1:x2], (224, 224))
+            image_array = np.asarray(crop_A)
+            normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+            data[0] = normalized_image_array
+            interpreter.set_tensor(entrada_details[0]['index'], data)
+            interpreter.invoke()
+            saida = interpreter.get_tensor(saida_details[0]['index'])
+            classe_predita = np.argmax(saida)
+            if classe_predita == 0:
+                cv.rectangle(image, (x1,y1+1), (x2,y2-1), (0,0,255), 2)
+                red_count += 1
+            elif classe_predita == 1:
+                cv.rectangle(image, (x1,y1+1), (x2,y2-1), (0,255,0), 2)
+                green_count += 1
 
 
-        if i != 9: #Posição 9 das colunas C e D não são vagas
-            #Coluna C
-            cv.rectangle(image, (x1+distance_column_A_C,y1), (x2+distance_column_A_C,y2), (0,0,255), 2)
-            crop_C = cv.resize(image[y1:y2, x1+distance_column_A_C:x2+distance_column_A_C], (224, 224))
+            #Coluna B
+            crop_B = cv.resize(image[y1:y2, x2:x2+length_parking_space], (224, 224))
+            image_array = np.asarray(crop_B)
+            normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+            data[0] = normalized_image_array
+            interpreter.set_tensor(entrada_details[0]['index'], data)
+            interpreter.invoke()
+            saida = interpreter.get_tensor(saida_details[0]['index'])
+            classe_predita = np.argmax(saida)
+            if classe_predita == 0:
+                cv.rectangle(image, (x2,y1), (x2+length_parking_space,y2), (0,0,255), 2)
+                red_count += 1
+            elif classe_predita == 1:
+                cv.rectangle(image, (x2,y1), (x2+length_parking_space,y2), (0,255,0), 2)
+                green_count += 1
 
-            #Coluna D
-            cv.rectangle(image, (x2+distance_column_A_C,y1), (x2+distance_column_A_C+length_parking_space,y2), (0,0,255), 2)
-            crop_D = cv.resize(image[y1:y2, x2+distance_column_A_C:x2+distance_column_A_C+length_parking_space], (224, 224))
 
-        #Coluna E
-        cv.rectangle(image, (x1+distance_column_A_E,y1), (x2+distance_column_A_E,y2), (0,0,255), 2)
-        crop_E = cv.resize(image[y1:y2, x1+distance_column_A_E:x2+distance_column_A_E], (224, 224))
+
+
+            if i != 9: #Posição 9 das colunas C e D não são vagas
+                #Coluna C
+                crop_C = cv.resize(image[y1:y2, x1+distance_column_A_C:x2+distance_column_A_C], (224, 224))
+                image_array = np.asarray(crop_C)
+                normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+                data[0] = normalized_image_array
+                interpreter.set_tensor(entrada_details[0]['index'], data)
+                interpreter.invoke()
+                saida = interpreter.get_tensor(saida_details[0]['index'])
+                classe_predita = np.argmax(saida)
+                if classe_predita == 0:
+                    cv.rectangle(image, (x1+distance_column_A_C,y1), (x2+distance_column_A_C,y2), (0,0,255), 2)                
+                    red_count += 1
+                elif classe_predita == 1:
+                    cv.rectangle(image, (x1+distance_column_A_C,y1), (x2+distance_column_A_C,y2), (0,255,0), 2)                
+                    green_count += 1
+                
+
+                #Coluna D
+                crop_D = cv.resize(image[y1:y2, x2+distance_column_A_C:x2+distance_column_A_C+length_parking_space], (224, 224))
+                image_array = np.asarray(crop_D)
+                normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+                data[0] = normalized_image_array
+                interpreter.set_tensor(entrada_details[0]['index'], data)
+                interpreter.invoke()
+                saida = interpreter.get_tensor(saida_details[0]['index'])
+                classe_predita = np.argmax(saida)
+                if classe_predita == 0:
+                    cv.rectangle(image, (x2+distance_column_A_C,y1), (x2+distance_column_A_C+length_parking_space,y2), (0,0,255), 2)
+                    red_count += 1
+                elif classe_predita == 1:
+                    cv.rectangle(image, (x2+distance_column_A_C,y1), (x2+distance_column_A_C+length_parking_space,y2), (0,255,0), 2)
+                    green_count += 1
+                
+                
+
+            #Coluna E
+            crop_E = cv.resize(image[y1:y2, x1+distance_column_A_E:x2+distance_column_A_E], (224, 224))
+            image_array = np.asarray(crop_E)
+            normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+            data[0] = normalized_image_array
+            interpreter.set_tensor(entrada_details[0]['index'], data)
+            interpreter.invoke()
+            saida = interpreter.get_tensor(saida_details[0]['index'])
+            classe_predita = np.argmax(saida)
+            if classe_predita == 0:
+                cv.rectangle(image, (x1+distance_column_A_E,y1), (x2+distance_column_A_E,y2), (0,0,255), 2)
+                red_count += 1
+            elif classe_predita == 1:
+                cv.rectangle(image, (x1+distance_column_A_E,y1), (x2+distance_column_A_E,y2), (0,255,0), 2)
+                green_count += 1
+                    
+            
+            if i != 1: #Na coluna F, posição, não há uma vaga
+                #Coluna F
+                crop_F = cv.resize(image[y1:y2, x2+distance_column_A_E+distance_column_E_F:x2+distance_column_A_E+length_parking_space+distance_column_E_F], (224, 224))
+                image_array = np.asarray(crop_F)
+                normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
+                data[0] = normalized_image_array
+                interpreter.set_tensor(entrada_details[0]['index'], data)
+                interpreter.invoke()
+                saida = interpreter.get_tensor(saida_details[0]['index'])
+                classe_predita = np.argmax(saida)
+                if classe_predita == 0:
+                    cv.rectangle(image, (x2+distance_column_A_E+distance_column_E_F,y1), (x2+distance_column_A_E+length_parking_space+distance_column_E_F,y2), (0,0,255), 2)
+                    red_count += 1
+                elif classe_predita == 1:
+                    cv.rectangle(image, (x2+distance_column_A_E+distance_column_E_F,y1), (x2+distance_column_A_E+length_parking_space+distance_column_E_F,y2), (0,255,0), 2)
+                    green_count += 1
+
+
+            #Rotinas de rotulação de cada coluna do estacionamento
+            cv.putText(image, f"a{i}", (x1+put_text_x1,y1+put_text_y), cv.FONT_HERSHEY_SIMPLEX, 0.5, (51, 255, 255), 1)
+            cv.putText(image, f"b{i}", (x2+put_text_x2,y1+put_text_y), cv.FONT_HERSHEY_SIMPLEX, 0.5, (51, 255, 255), 1)
+            if i != 9:
+                cv.putText(image, f"c{i}", (x1+distance_column_A_C+put_text_x1,y1+put_text_y), cv.FONT_HERSHEY_SIMPLEX, 0.5, (51, 255, 255), 1)
+                cv.putText(image, f"d{i}", (x2+distance_column_A_C+put_text_x2,y1+put_text_y), cv.FONT_HERSHEY_SIMPLEX, 0.5, (51, 255, 255), 1)
+            cv.putText(image, f"e{i}", (x1+distance_column_A_E+put_text_x1,y1+put_text_y), cv.FONT_HERSHEY_SIMPLEX, 0.5, (51, 255, 255), 1)
+            if i != 1:
+                cv.putText(image, f"f{i}", (x2+distance_column_A_E+put_text_x2+distance_column_E_F,y1+put_text_y), cv.FONT_HERSHEY_SIMPLEX, 0.5, (51, 255, 255), 1)
+
+            #Atualização dos valores das ordenadas das vagas
+            y1 += 47
+            y2 += 47
         
-        if i != 1: #Na coluna F, posição, não há uma vaga
-            #Coluna F
-            cv.rectangle(image, (x2+distance_column_A_E+distance_column_E_F,y1), (x2+distance_column_A_E+length_parking_space+distance_column_E_F,y2), (0,0,255), 2)
-            crop_F = cv.resize(image[y1:y2, x2+distance_column_A_E+distance_column_E_F:x2+distance_column_A_E+length_parking_space+distance_column_E_F], (224, 224))
-
-        
-        #Rotinas de rotulação de cada coluna do estacionamento
-        cv.putText(image, f"a{i}", (x1+put_text_x1,y1+put_text_y), cv.FONT_HERSHEY_SIMPLEX, 0.5, (51, 255, 255), 1)
-        cv.putText(image, f"b{i}", (x2+put_text_x2,y1+put_text_y), cv.FONT_HERSHEY_SIMPLEX, 0.5, (51, 255, 255), 1)
-        if i != 9:
-            cv.putText(image, f"c{i}", (x1+distance_column_A_C+put_text_x1,y1+put_text_y), cv.FONT_HERSHEY_SIMPLEX, 0.5, (51, 255, 255), 1)
-            cv.putText(image, f"d{i}", (x2+distance_column_A_C+put_text_x2,y1+put_text_y), cv.FONT_HERSHEY_SIMPLEX, 0.5, (51, 255, 255), 1)
-        cv.putText(image, f"e{i}", (x1+distance_column_A_E+put_text_x1,y1+put_text_y), cv.FONT_HERSHEY_SIMPLEX, 0.5, (51, 255, 255), 1)
-        if i != 1:
-            cv.putText(image, f"f{i}", (x2+distance_column_A_E+put_text_x2+distance_column_E_F,y1+put_text_y), cv.FONT_HERSHEY_SIMPLEX, 0.5, (51, 255, 255), 1)
-
-        #Atualização dos valores das ordenadas das vagas
-        y1 += 47
-        y2 += 47
-        
-
-    cv.imshow('Video', image)
+        cv.imshow('Video', image)
 
     #time.sleep(0.02)
+    frame_count += 1
+
 
     #Rotina para parar a execução caso o usuário pressione a tecla ESC
     k = cv.waitKey(1) & 0xFF
